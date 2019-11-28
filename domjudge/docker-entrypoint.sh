e129bbf03b3043642d9452688b699eff7e2b1379
mnb3000 <misha.mykolyshyn@gmail.com>
2019-11-29 00:57:40 +0200

    Added start script to entrypoint

--- domjudge/docker-entrypoint.sh
+++ domjudge/docker-entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/opt/domjudge/judgehost/bin/dj_make_chroot -d /chroot/domjudge -i mono-devel,mono-mcs,nodejs -y -D Ubuntu -R bionic -a amd64
+[ ! -d "/chroot/domjudge" ] && /opt/domjudge/judgehost/bin/dj_make_chroot -d /chroot/domjudge -i mono-devel,mono-mcs,nodejs -y -D Ubuntu -R bionic -a amd64
+/scripts/start.sh
