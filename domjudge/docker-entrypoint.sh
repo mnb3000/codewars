7019e7e3cb08fa9d7f19232347e2d199c0101e25
mnb3000 <misha.mykolyshyn@gmail.com>
2019-11-29 01:09:06 +0200

    Added full mono installation '

--- domjudge/docker-entrypoint.sh
+++ domjudge/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-[ ! -d "/chroot/domjudge" ] && /opt/domjudge/judgehost/bin/dj_make_chroot -d /chroot/domjudge -i mono-devel,mono-mcs,nodejs -y -D Ubuntu -R bionic -a amd64
+[ ! -d "/chroot/domjudge" ] && /opt/domjudge/judgehost/bin/dj_make_chroot -d /chroot/domjudge -i mono-complete,nodejs -y -D Ubuntu -R bionic -a amd64
 /scripts/start.sh
